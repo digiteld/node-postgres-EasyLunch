64c1471d700bf4999198590229ed72563c12fe42
jb <jbaptiste.daniel@gmail.com>
2018-02-01 14:00:13 +0100

    fix cross domain

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 app.use(allowCrossDomain);
 app.use(logger('dev'));
 app.use(bodyParser.json());
