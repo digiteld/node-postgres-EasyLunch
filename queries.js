4b3e9523e55d190cb0713175127131c4b0a47e06
romain Focone <email@example.com>
2018-02-06 09:59:16 +0100

    add request and update gitignore

--- queries.js
+++ queries.js
@@ -9,109 +9,544 @@ var pgp = require('pg-promise')(options);
 //var connectionString = 'postgres://localhost:5432/puppies';
 var connectionString = 'postgres://rdythibocmspfq:28d97c027abdd748c1b93c11b7063de6ff4106acd6fa3d6df24559913283f536@ec2-54-247-101-191.eu-west-1.compute.amazonaws.com:5432/d8qspi9bf1ncj3';
 var db = pgp(connectionString);
+pgp.pg.defaults.ssl = true;
 
-function getAllPuppies(req, res, next) {
-  db.any('select * from pups')
-    .then(function (data) {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Retrieved ALL puppies'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
+
+// BOOKING
+
+function getAllBookings(req, res, next) {
+  db.any('select * from booking')
+  .then(function (data) {
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ALL bookings'
     });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
 }
+
+function getSingleBooking(req, res, next) {
+  var bookingID = parseInt(req.params.id);
+  db.one('select * from booking where id = $1', bookingID)
+  .then(function (data) {
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ONE booking'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function createBooking(req, res, next) {
+
+  db.none('insert into booking(master_user_id, restaurant_id, nb_users, schedule, created_date, updated_date)' +
+  'values(${master_user_id}, ${restaurant_id}, ${nb_users}, ${schedule}, ${created_date}, ${updated_date})',
+  req.body)
+  .then(function () {
+    res.status(200)
+    .json({
+      status: 'success',
+      message: 'Inserted one booking'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function updateBooking(req, res, next) {
+  db.none('update booking set master_user_id=$1, restaurantID=$2, nb_users=$3, schedule=$4, created_date=$5, updated_date=$6 where id=$7',
+  [req.body.master_user_id, req.body.restaurantID, req.body.nb_users, req.body.schedule, req.body.created_date, req.body.updated_date])
+  .then(function () {
+    res.status(200)
+    .json({
+      status: 'success',
+      message: 'Updated booking'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function removeBooking(req, res, next) {
+  var commandID = parseInt(req.params.id);
+  db.result('delete from command where id = $1', commandID)
+  .then(function (result) {
+    /* jshint ignore:start */
+    res.status(200)
+    .json({
+      status: 'success',
+      message: `Removed ${result.rowCount} command`
+    });
+    /* jshint ignore:end */
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+
+
+// COMMAND
+
+function getAllCommands(req, res, next) {
+  db.any('select * from command')
+  .then(function (data) {
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ALL Commands'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function getSingleCommand(req, res, next) {
+  var commandID = parseInt(req.params.id);
+  db.one('select * from command where id = $1', commandID)
+  .then(function (data) {
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ONE command'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function createCommand(req, res, next) {
+
+  db.none('insert into command(user_id, booking_id, meal_id, payment_id, created_date, updated_date)' +
+  'values(${user_id}, ${booking_id}, ${meal_id}, ${payment_id}, ${created_date}, ${updated_date})',
+  req.body)
+  .then(function () {
+    res.status(200)
+    .json({
+      status: 'success',
+      message: 'Inserted one command'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function updateCommand(req, res, next) {
+  db.none('update command set user_id=$1, booking_id=$2, meal_id=$3, payment_id=$4, created_date=$5, updated_date=$6 where id=$7',
+  [req.body.user_id, req.body.booking_id, req.body.meal_id, req.body.payment_id, req.body.created_date, req.body.updated_date])
+  .then(function () {
+    res.status(200)
+    .json({
+      status: 'success',
+      message: 'Updated command'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function removeCommand(req, res, next) {
+  var commandID = parseInt(req.params.id);
+  db.result('delete from command where id = $1', commandID)
+  .then(function (result) {
+    /* jshint ignore:start */
+    res.status(200)
+    .json({
+      status: 'success',
+      message: `Removed ${result.rowCount} command`
+    });
+    /* jshint ignore:end */
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+
+
+
+// MEALS
+
+function getAllMeals(req, res, next) {
+  db.any('select * from meals')
+  .then(function (data) {
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ALL meals'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function getSingleMeal(req, res, next) {
+  var mealID = parseInt(req.params.id);
+  db.one('select * from meals where id = $1', mealID)
+  .then(function (data) {
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ONE meal'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function createMeal(req, res, next) {
+
+  db.none('insert into meals(restaurant_id, name, description, price, created_date, updated_date)' +
+  'values(${restaurant_id}, ${name}, ${description}, ${price}, ${created_date}, ${updated_date})',
+  req.body)
+  .then(function () {
+    res.status(200)
+    .json({
+      status: 'success',
+      message: 'Inserted one meal'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function updateMeal(req, res, next) {
+  db.none('update meals set restaurant_id=$1, name=$2, description=$3, price=$4, created_date=$5, updated_date=$6 where id=$7',
+  [req.body.restaurant_id, req.body.name, req.body.description, req.body.price, req.body.created_date, req.body.updated_date])
+  .then(function () {
+    res.status(200)
+    .json({
+      status: 'success',
+      message: 'Updated meal'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function removeMeal(req, res, next) {
+  var mealID = parseInt(req.params.id);
+  db.result('delete from meals where id = $1', mealID)
+  .then(function (result) {
+    /* jshint ignore:start */
+    res.status(200)
+    .json({
+      status: 'success',
+      message: `Removed ${result.rowCount} meal`
+    });
+    /* jshint ignore:end */
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+// PAYMENT
+
+function getAllPayments(req, res, next) {
+  db.any('select * from payment')
+  .then(function (data) {
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ALL payments'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function getSinglePayment(req, res, next) {
+  var paymentID = parseInt(req.params.id);
+  db.one('select * from payment where id = $1', paymentID)
+  .then(function (data) {
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ONE payment'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function createPayment(req, res, next) {
+
+  db.none('insert into payment(amount, command_id, status, user_id, created_date, updated_date, stripe_id)' +
+  'values(${amount}, ${command_id}, ${status}, ${user_id}, ${created_date}, ${updated_date}, ${stripe_id})',
+  req.body)
+  .then(function () {
+    res.status(200)
+    .json({
+      status: 'success',
+      message: 'Inserted one payment'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function updatePayment(req, res, next) {
+  db.none('update payment set amount=$1, command_id=$2, status=$3, user_id=$4, created_date=$5, updated_date=$6, stripe_id=$7 where id=$8',
+  [req.body.amount, req.body.command_id, req.body.status, req.body.user_id, req.body.created_date, req.body.updated_date, req.body.stripe_id])
+  .then(function () {
+    res.status(200)
+    .json({
+      status: 'success',
+      message: 'Updated payment'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function removePayment(req, res, next) {
+  var paymentID = parseInt(req.params.id);
+  db.result('delete from payment where id = $1', paymentID)
+  .then(function (result) {
+    /* jshint ignore:start */
+    res.status(200)
+    .json({
+      status: 'success',
+      message: `Removed ${result.rowCount} payment`
+    });
+    /* jshint ignore:end */
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+// RESTAURANTS
 
 function getAllRestaurants(req, res, next) {
   db.any('select * from restaurants')
-    .then(function (data) {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Retrieved ALL restaurants'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
+  .then(function (data) {
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ALL restaurants'
     });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function getSingleRestaurant(req, res, next) {
+  var restaurantID = parseInt(req.params.id);
+  db.one('select * from restaurants where id = $1', restaurantID)
+  .then(function (data) {
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ONE restaurant'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function createRestaurant(req, res, next) {
+
+  db.none('insert into restaurants(name, description, lat, lon, address, schedule, person_capacity, created_date, location, picture)' +
+  'values(${name}, ${description}, ${lat}, ${lon}, ${address}, ${schedule}, ${person_capacity}, ${created_date}, ${location}, ${picture})',
+  req.body)
+  .then(function () {
+    res.status(200)
+    .json({
+      status: 'success',
+      message: 'Inserted one restaurant'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function updateRestaurant(req, res, next) {
+  db.none('update restaurants set name=$1, description=$2, lat=$3, lon=$4, address=$5, schedule=$6, person_capacity=$7, created_date=$8, location=$9, picture=$10 where id=$11',
+  [req.body.name, req.body.description, req.body.lat, req.body.lon, req.body.address, req.body.schedule, req.body.person_capacity, req.body.created_date, req.body.location, req.body.picture])
+  .then(function () {
+    res.status(200)
+    .json({
+      status: 'success',
+      message: 'Updated restaurant'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+function removeRestaurant(req, res, next) {
+  var restaurantID = parseInt(req.params.id);
+  db.result('delete from restaurants where id = $1', restaurantID)
+  .then(function (result) {
+    /* jshint ignore:start */
+    res.status(200)
+    .json({
+      status: 'success',
+      message: `Removed ${result.rowCount} restaurant`
+    });
+    /* jshint ignore:end */
+  })
+  .catch(function (err) {
+    return next(err);
+  });
+}
+
+//PUPS
+
+function getAllPuppies(req, res, next) {
+  db.any('select * from pups')
+  .then(function (data) {
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ALL puppies'
+    });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
 }
 
 function getSinglePuppy(req, res, next) {
   var pupID = parseInt(req.params.id);
   db.one('select * from pups where id = $1', pupID)
-    .then(function (data) {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Retrieved ONE puppy'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
+  .then(function (data) {
+    res.status(200)
+    .json({
+      status: 'success',
+      data: data,
+      message: 'Retrieved ONE puppy'
     });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
 }
 
 function createPuppy(req, res, next) {
   req.body.age = parseInt(req.body.age);
   db.none('insert into pups(name, breed, age, sex)' +
-      'values(${name}, ${breed}, ${age}, ${sex})',
-    req.body)
-    .then(function () {
-      res.status(200)
-        .json({
-          status: 'success',
-          message: 'Inserted one puppy'
-        });
-    })
-    .catch(function (err) {
-      return next(err);
+  'values(${name}, ${breed}, ${age}, ${sex})',
+  req.body)
+  .then(function () {
+    res.status(200)
+    .json({
+      status: 'success',
+      message: 'Inserted one puppy'
     });
+  })
+  .catch(function (err) {
+    return next(err);
+  });
 }
 
 function updatePuppy(req, res, next) {
   db.none('update pups set name=$1, breed=$2, age=$3, sex=$4 where id=$5',
-    [req.body.name, req.body.breed, parseInt(req.body.age),
-      req.body.sex, parseInt(req.params.id)])
+  [req.body.name, req.body.breed, parseInt(req.body.age),
+    req.body.sex, parseInt(req.params.id)])
     .then(function () {
       res.status(200)
-        .json({
-          status: 'success',
-          message: 'Updated puppy'
-        });
+      .json({
+        status: 'success',
+        message: 'Updated puppy'
+      });
     })
     .catch(function (err) {
       return next(err);
     });
-}
+  }
 
-function removePuppy(req, res, next) {
-  var pupID = parseInt(req.params.id);
-  db.result('delete from pups where id = $1', pupID)
+  function removePuppy(req, res, next) {
+    var pupID = parseInt(req.params.id);
+    db.result('delete from pups where id = $1', pupID)
     .then(function (result) {
       /* jshint ignore:start */
       res.status(200)
-        .json({
-          status: 'success',
-          message: `Removed ${result.rowCount} puppy`
-        });
+      .json({
+        status: 'success',
+        message: `Removed ${result.rowCount} puppy`
+      });
       /* jshint ignore:end */
     })
     .catch(function (err) {
       return next(err);
     });
-}
+  }
 
 
-module.exports = {
-  getAllPuppies: getAllPuppies,
-  getAllRestaurants: getAllRestaurants,
-  getSinglePuppy: getSinglePuppy,
-  createPuppy: createPuppy,
-  updatePuppy: updatePuppy,
-  removePuppy: removePuppy
-};
+  module.exports = {
+
+    getAllBookings: getAllBookings,
+    getSingleBooking: getSingleBooking,
+    createBooking: createBooking,
+    updateBooking: updateBooking,
+    removeBooking: removeBooking,
+
+    getAllCommands: getAllCommands,
+    getSingleCommand: getSingleCommand,
+    createCommand: createCommand,
+    updateCommand: updateCommand,
+    removeCommand: removeCommand,
+
+    getAllMeals: getAllMeals,
+    getSingleMeal: getSingleMeal,
+    createMeal: createMeal,
+    updateMeal: updateMeal,
+    removeMeal: removeMeal,
+
+    getAllRestaurants: getAllRestaurants,
+    getSingleRestaurant: getSingleRestaurant,
+    createRestaurant: createRestaurant,
+    updateRestaurant: updateRestaurant,
+    removeRestaurant: removeRestaurant,
+
+    getAllPayments: getAllPayments,
+    getSinglePayment: getSinglePayment,
+    createPayment: createPayment,
+    updatePayment: updatePayment,
+    removePayment: removePayment,
+
+    getAllPuppies: getAllPuppies,
+    getSinglePuppy: getSinglePuppy,
+    createPuppy: createPuppy,
+    updatePuppy: updatePuppy,
+    removePuppy: removePuppy
+
+  };
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,40 @@ var db = require('../queries');
 
 
 router.get('/api/puppies', db.getAllPuppies);
-router.get('/api/restaurants', db.getAllRestaurants);
 router.get('/api/puppies/:id', db.getSinglePuppy);
 router.post('/api/puppies', db.createPuppy);
 router.put('/api/puppies/:id', db.updatePuppy);
 router.delete('/api/puppies/:id', db.removePuppy);
+
+router.get('/api/restaurants', db.getAllRestaurants);
+router.get('/api/restaurants/:id', db.getSingleRestaurant);
+router.post('/api/restaurants', db.createRestaurant);
+router.put('/api/restaurants/:id', db.updateRestaurant);
+router.delete('/api/restaurants/:id', db.removeRestaurant);
+
+router.get('/api/booking', db.getAllBookings);
+router.get('/api/booking/:id', db.getSingleBooking);
+router.post('/api/booking', db.createBooking);
+router.put('/api/booking/:id', db.updateBooking);
+router.delete('/api/booking/:id', db.removeBooking);
+
+router.get('/api/command', db.getAllCommands);
+router.get('/api/command/:id', db.getSingleCommand);
+router.post('/api/command', db.createCommand);
+router.put('/api/command/:id', db.updateCommand);
+router.delete('/api/command/:id', db.removeCommand);
+
+router.get('/api/meal', db.getAllMeals);
+router.get('/api/meal/:id', db.getSingleMeal);
+router.post('/api/meal', db.createMeal);
+router.put('/api/meal/:id', db.updateMeal);
+router.delete('/api/meal/:id', db.removeMeal);
+
+router.get('/api/payment', db.getAllPayments);
+router.get('/api/payment/:id', db.getSinglePayment);
+router.post('/api/payment', db.createPayment);
+router.put('/api/payment/:id', db.updatePayment);
+router.delete('/api/payment/:id', db.removePayment);
 
 // application -------------------------------------------------------------
 router.get('/', function (req, res) {
